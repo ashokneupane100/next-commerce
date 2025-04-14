a7540c5fd841143c713070a122df8da862358954
Ashok Neupane <ashokabrother@gmail.com>
2025-04-14 12:11:42 +0545

    Learning the component structure and props

--- app/page.js
+++ app/page.js
@@ -1,11 +1,25 @@
 import Image from "next/image";
 
-export default function Home() {
-  const name="Ashok "
+function Card(props) {
   return (
-    < >
-      <div className="bg-green-700">Hello  {name}</div>
+    <div className="border rounded-md border-gray-600 p-4">Card Component:{props.text}</div>
+  );
+}
+
+export default function Home() {
+  const name = "Ashok ";
+  return (
+    <>
+      <div className="p-20 space-y-4">
+        <div className="">Hello {name}</div>
+        <Card text="This is being passed from the parent."/>
+        <Card/>
+        <Card/>
+        <Card/>
+        <Card/>
+        <Card/>
+        <Card/>
+      </div>
     </>
-    
   );
 }
