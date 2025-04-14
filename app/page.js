91f895d3b7eb19028ceb00492adbfd858ba54983
Ashok Neupane <ashokabrother@gmail.com>
2025-04-14 14:38:45 +0545

    reactJS recap done for memorizing

--- app/page.js
+++ app/page.js
@@ -1,34 +1,38 @@
 'use client'
 
 import { useState } from "react"
+import Card from "@/components/card"
 
-function Card({ children }) {
-  return (
-    <div className="border rounded-md border-gray-600 p-4">
-      {children}
-    </div>
-  )
-}
+
 
 export default function Home() {
   const [isVisible, setIsVisible] = useState(true)
-  const name = 'Ashok'
+  const [names, setNames] = useState(['Piotr', 'John', 'Terry'])
+  const name = 'Piotr'
   const handleClick = () => {
     setIsVisible(!isVisible)
-    
   }
-  
+
+  const handleAdd=()=>{
+    setNames([...names,'new Elements'])
+
+  }
+ 
 
 
-
+  const cards = isVisible
+    && names.map((name, index) => <Card key={index}>{name}</Card>)
   return (
     <>
       <div className="p-20 space-y-4">
         <div>Hello, {name}</div>
-        {isVisible && <><Card>This is being passed from the parent.</Card></>}
-      
-
-        <button onClick={handleClick}>{isVisible ? "Hide" :"Show"}</button>
+        {cards}
+        <div className="flex space-x-4">
+          <button onClick={handleClick}>
+            {isVisible ? 'Hide' : 'Show'}
+          </button>
+      <button onClick={handleAdd}>Add</button>
+        </div>
       </div>
     </>
   )
