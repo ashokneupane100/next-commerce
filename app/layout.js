318431bafa9ebb5e63f7d72dedacb79dbb764a15
Ashok Neupane <ashokabrother@gmail.com>
2025-04-14 16:59:59 +0545

    17. Links and Navigation

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/header";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -22,9 +23,7 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <header className="p-20 border border-yellow-400">
-          Iam  visible to everywhere.
-        </header>
+       <Header/>
        <main>
        {children}
        </main>
