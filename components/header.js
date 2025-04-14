a345d0280d80ba5cd9092a0f4a768089b993364a
Ashok Neupane <ashokabrother@gmail.com>
2025-04-14 18:16:07 +0545

    18. Understand Prefetching and Caching

--- components/header.js
+++ components/header.js
@@ -1,18 +1,22 @@
-import React from 'react'
-import Counter from './counter'
-import Link from 'next/link'
-
+import React from "react";
+import Counter from "./counter";
+import Link from "next/link";
 
 export default function Header() {
   return (
     <header className="p-20 border border-yellow-400">
-   <ul className="flex space-x-4">
-    <li><Link href="/">Home</Link></li>
-    <li><Link href="/about">About</Link></li>
-    <li><Link href="/about/projects">Projects</Link></li>
-
-  </ul>
-  <Counter/>
-  </header>
-  )
+      <ul className="flex space-x-4">
+        <li>
+          <Link href="/">Home</Link>
+        </li>
+        <li>
+          <Link href="/about">About</Link>
+        </li>
+        <li>
+          <Link href="/about/projects">Projects</Link>
+        </li>
+      </ul>
+      <Counter />
+    </header>
+  );
 }
