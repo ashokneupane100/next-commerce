c8bf710153e2ac0cf81281b7de84b74dde080db8
Ashok Neupane <ashokabrother@gmail.com>
2025-04-14 13:24:32 +0545

    10. Keeping Component State using Hooks (useState)

--- app/page.js
+++ app/page.js
@@ -1,40 +1,36 @@
-"use client"
-import Image from "next/image";
+'use client'
 
-function Card({children}) {
+import { useState } from "react"
+
+function Card({ children }) {
   return (
-    <div className="border rounded-md border-gray-600 p-4">Card Component:{children}</div>
-  );
+    <div className="border rounded-md border-gray-600 p-4">
+      {children}
+    </div>
+  )
 }
 
 export default function Home() {
-  const handleClick=(e)=>{
-    e.preventDefault();
-    console.log(e)
-    alert("Annapurna is my lovely daughter. But today she is being lazy !")
- }
-  const name = "Ashok ";
+  const [label, setLabel] = useState('Show')
+  const name = 'Piotr'
+  const handleClick = () => {
+    setLabel(label == 'Show' ? 'Hide' : 'Show')
+  }
   return (
     <>
       <div className="p-20 space-y-4">
-        <div className="">Hello {name}</div>
-        <Card >This is passed to child component !</Card>
-        <Card >
-          <div className="">This is JS.</div>
+        <div>Hello, {name}</div>
+        <Card>This is being passed from the parent</Card>
+        <Card>
+          <div>This is JS!</div>
           <Card>Nested text!</Card>
         </Card>
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
+        <Card />
+        <Card />
+        <Card />
 
-<button onClick={handleClick} className="inline-flex items-center justify-center px-8 py-4 font-sans font-semibold tracking-wide text-white bg-blue-500 rounded-lg h-[60px]">
-  Click me!
-</button>
-
-
+        <button onClick={handleClick}>{label}</button>
       </div>
     </>
-  );
+  )
 }
